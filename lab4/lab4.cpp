49c25c916e996b543ab294f2059894e39119a13e
Sergey Dmitrievich <serj_1313@mail.ru>
2024-03-27 23:05:23 +0300

    lab4: правлен алгоритм и написан отчет

--- lab4/lab4.cpp
+++ lab4/lab4.cpp
@@ -1,6 +1,7 @@
 #include <cmath>
 #include <numbers>
 #include <iostream>
+#include <iomanip>
 
 #include "../lab1/lab1.cpp"
 
@@ -10,17 +11,15 @@
 // y = eˣ, x ∈ [0, 1], y(0)=e⁰=1, y(1)=e
 // y"(x) + 5y'(x) - 4y(x) = 2eˣ
 
-// в отчет вынести два столбика значений y(xᵢ) и yᵢ(x)
-
-#define N 50
+#define N 10
 
 auto main() -> int {
-    auto y = [](double x){return std::numbers::e;};
+    auto y = [](double x){return std::pow(std::numbers::e, x);};
 
-    double a = 1;
-    double b = std::numbers::e;
+    double a = y(0);
+    double b = y(1);
 
-    // y"(x) + p(x)*y'(x) - q(x)*y(x) = f(x)
+    // y"(x) + p(x)*y'(x) + q(x)*y(x) = f(x)
     auto p_x = [](double x){return 5;};
     auto q_x = [](double x){return -4;};
     auto f_x = [](double x){return 2 * std::pow(std::numbers::e, x);};
@@ -46,23 +45,35 @@ auto main() -> int {
         SLAE.at(i - 1, i - 1) = h * h * q_i(i) - 2;
         SLAEright.at(i - 1) = h * h * f_i(i);
     }
+    SLAEright.at(0) -= a * (1 - h / 2 * p_i(1));
+    SLAEright.at(N - 2) -= b * (1 + h / 2 * p_i(N - 1));
     Vector<double, N - 1> SLAEsolve = findSolve(SLAE, SLAEright);
 
     Vector<double, N + 1> y_i{};
     y_i.at(0) = a;
     y_i.at(N) = b;
-
     for (std::size_t i = 1; i != N; ++i) {
         y_i.at(i) = SLAEsolve.at(i - 1);
     }
 
     // ||y - y˜|| = max|y(xᵢ) - yᵢ|
-    double solve_error = 0;
+    // double solve_error = 0;
+    std::cout
+        << std::setw(4) << " i"
+        << std::setw(12) << " y(x_i)"
+        << std::setw(12) << " y_i"
+        << std::setw(16) << " |y(x_i) - y_i|" << '\n';
+    std::cout << std::setw(45) << std::setfill('-') << '\n';
+    std::cout << std::setfill(' ');
     for (std::size_t i = 0; i != N + 1; ++i) {
-        solve_error = std::max(solve_error, std::abs(y(x_i(i)) - y_i.at(i)));
+        double error_i = std::abs(y(x_i(i)) - y_i.at(i));
+        // solve_error = std::max(solve_error, error_i);
+        std::cout
+            << std::setw(4) << i
+            << std::setw(12) << y(x_i(i))
+            << std::setw(12) << y_i.at(i)
+            << std::setw(16) << error_i << '\n';
     }
-
-    std::cout << "Numeral solve error = " << solve_error << '\n';
 
     return 0;
 }
